71e383037176e182c98899e7e526aff626b4ad44
Florian Chis <florian.chis@3pillarglobal.com>
2014-06-20 11:44:55 +0300

    fixed __init__

--- colors/__init__.py
+++ colors/__init__.py
@@ -1,6 +1,19 @@
 VERSION = (0, 0, 1, 'alpha', 0)
 
+def get_git_revision():
+    #TODO
+    pass
+
 def get_version():
-    version = VERSION
-    #TODO maybe attach git head revision number
+    version = '%s.%s' % (VERSION[0], VERSION[1])
+    if VERSION[2]:
+        version = '%s.%s' % (version, VERSION[2])
+    if VERSION[3:] == ('alpha', 0):
+        version = '%s pre-alpha' % version
+    else:
+        if VERSION[3] != 'final':
+            version = '%s %s %s' % (version, VERSION[3], VERSION[4])
+    git_rev = get_git_revision()
+    if git_rev:
+        version = "%s %s" % (version, git_rev)
     return version
